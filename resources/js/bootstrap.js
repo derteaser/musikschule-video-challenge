3fa2e8ba591bb655e6ca8f92c86943d67314f435
Jens Küsters <jens@kuesters.net>
2021-04-08 08:55:42 +0200

    removed pikaday date picker

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-window.moment = require('moment');
-window.Pikaday = require('pikaday');

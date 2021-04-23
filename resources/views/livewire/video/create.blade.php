c5094297dea351fbd920c4e7e8c9c71c79f02f5c
Jens Küsters <jens@kuesters.net>
2021-04-23 11:10:44 +0200

    added loading indicator for upload widget

--- resources/views/livewire/video/create.blade.php
+++ resources/views/livewire/video/create.blade.php
@@ -2,7 +2,8 @@
     function upload() {
         return {
             uploadWidget: null,
-            buttonVisible: true,
+            buttonVisible: false,
+            placeholderVisible: true,
             init: function() {
                 this.uploadWidget = cloudinary.openUploadWidget({
                     cloudName: @json(Str::after(config('cloudinary.cloud_url'),'@')),
@@ -56,13 +57,15 @@
                     }
                 }, (error, result) => {
                     if (result.event === "success") {
+                        this.placeholderVisible = true;
+                        this.buttonVisible = false;
                         @this.createVideo(result.info.public_id, result.info.original_filename);
-                    }
-                    if (result.event === "display-changed") {
+                    } else if (result.event === "display-changed") {
                         switch (result.info) {
                             case "shown":
                             case "expanded":
                                 this.buttonVisible = false;
+                                this.placeholderVisible = false;
                                 break;
                             default:
                                 this.buttonVisible = true;
@@ -79,6 +82,13 @@
     }
 </script>
 
-<section x-data="upload()" x-init="init()">
-    <x-jet-button @click="uploadWidget.open();" x-show="buttonVisible === true" x-cloak>{{ __('Create new Video') }}</x-jet-button>
+<section x-data="upload()" x-init="init()" class="w-full">
+    <div class="bg-yellow-500 text-white px-4 py-2 rounded-lg w-full inline-flex items-center" x-show="placeholderVisible === true" x-cloak>
+        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        {{ __('Please wait a moment...') }}
+    </div>
+    <x-jet-button @click="uploadWidget.open();" class="mx-auto" x-show="buttonVisible === true && placeholderVisible === false" x-cloak>{{ __('Create new Video') }}</x-jet-button>
 </section>

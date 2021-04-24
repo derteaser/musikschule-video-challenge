74774bdd5dfc755c91b379e7a01047916bc8d447
Jens Küsters <jens@kuesters.net>
2021-04-24 12:36:20 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10 prose prose-lg mx-auto">
                 {{ \Illuminate\Mail\Markdown::parse(File::get('../resources/markdown/welcome.md')) }}
+
+                @if (Auth::user()->can('create video') && !Auth::user()->video()->first())
+                    <x-jet-button onclick="window.location='{{ route('dashboard-own-video') }}'">Zum Video-Upload</x-jet-button>
+                @endif
             </div>
         </div>
     </div>

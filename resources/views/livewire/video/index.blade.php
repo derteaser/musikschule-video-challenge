c8097f90b57652e23b8a911649ef82920ccfa783
Jens Küsters <jens@kuesters.net>
2021-04-28 11:30:28 +0200

    Update index.blade.php

--- resources/views/livewire/video/index.blade.php
+++ resources/views/livewire/video/index.blade.php
@@ -7,31 +7,31 @@
     @endif
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 container mx-auto">
         @foreach($videos as /* @var App\Models\Video $video */ $video)
-        <div class="bg-white overflow-hidden shadow-md sm:rounded-lg relative">
-            <a href="{{ route('dashboard-video', ['id' => $video->id]) }}" class="group">
-                <img class="w-full h-64 object-cover" src="{{ $video->thumbnail()->toUrl() }}" alt="Video">
-                <div class="absolute top-0 right-0 m-4 flex flex-row space-x-2">
-                    @if ($video->comment && Auth::user()->can('comment video'))
-                        <div class="p-2 bg-gray-600 text-white text-xs uppercase rounded-lg">Kommentiert</div>
-                    @endif
-                    @if ($video->isHidden)
-                        <div class="p-2 bg-red-600 text-white text-xs uppercase rounded-lg">Verborgen</div>
-                    @endif
+            <a href="{{ route('dashboard-video', ['id' => $video->id]) }}" class="bg-white overflow-hidden shadow-md sm:rounded-lg group flex flex-col justify-between">
+                <div>
+                    <figure class="relative">
+                        <img class="w-full h-64 object-cover" src="{{ $video->thumbnail()->toUrl() }}" alt="Video">
+                        <div class="absolute top-0 right-0 m-4 flex flex-row space-x-2">
+                            @if ($video->comment && Auth::user()->can('comment video'))
+                                <div class="p-2 bg-gray-600 text-white text-xs uppercase rounded-lg">Kommentiert</div>
+                            @endif
+                            @if ($video->isHidden)
+                                <div class="p-2 bg-red-600 text-white text-xs uppercase rounded-lg">Verborgen</div>
+                            @endif
+                        </div>
+                    </figure>
+                    <h1 class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 group-hover:underline px-6 pt-6">{{ $video->name }}</h1>
                 </div>
                 <div class="p-6">
-                    <h1 class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 group-hover:underline">{{ $video->name }}</h1>
-                    <div class="mt-4">
+                    <div class="flex items-center">
                         <div class="flex items-center">
-                            <div class="flex items-center">
-                                <img class="h-8 w-8 rounded-full object-cover" src="{{ $video->user()->profile_photo_url }}" alt="{{ $video->user()->displayName() }}" />
-                                <span class="mx-2 text-gray-700 font-semibold">{{ $video->user()->displayName() }}</span>
-                            </div>
-                            <span class="mx-1 text-gray-600 text-xs">{{ $video->created_at->format('d.m.Y') }}</span>
+                            <img class="h-8 w-8 rounded-full object-cover" src="{{ $video->user()->profile_photo_url }}" alt="{{ $video->user()->displayName() }}" />
+                            <span class="mx-2 text-gray-700 font-semibold">{{ $video->user()->displayName() }}</span>
                         </div>
+                        <span class="mx-1 text-gray-600 text-xs">{{ $video->created_at->format('d.m.Y') }}</span>
                     </div>
                 </div>
             </a>
-        </div>
         @endforeach
     </div>
     <div class="mt-8">{{ $videos->links() }}</div>

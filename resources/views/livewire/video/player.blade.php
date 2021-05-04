b079433f3d192fe4cc42eb1ec6db8afd334a22d9
Jens Küsters <jens@kuesters.net>
2021-05-04 10:26:30 +0200

    Update player.blade.php

--- resources/views/livewire/video/player.blade.php
+++ resources/views/livewire/video/player.blade.php
@@ -1,3 +1,10 @@
 @php
-    echo cloudinary()->getVideoTag($video->cloudinary_public_id)->setAttributes(['controls', 'preload'])->fallback('Your browser does not support HTML5 video tags.')->scale(1600, 900);
+    /** @var \App\Models\Video $video */
 @endphp
+<div class="embed-responsive aspect-ratio-16/9 bg-gray-900">
+    <iframe
+        src="https://player.cloudinary.com/embed/?cloud_name={{ Str::after(config('cloudinary.cloud_url'),'@') }}&public_id={{ $video->video()->getPublicId() }}&controls=true"
+        class="embed-responsive-item"
+        allow="fullscreen; encrypted-media"
+    ></iframe>
+</div>
